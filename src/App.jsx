2f17fe083f2df7ad6282cce09f4287a94b97a7fb
Mimansa Joshi <mimansaj@vconstruct.in>
2026-02-04 16:25:53 +0530

    solution: React Fundamentals: Question 4 – Mini Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import './App.css'
+import ProductList from "./components/Products/ProductList";
+import './App.css';
+import Loader from "./components/Loader";
+import { useState } from "react";
 
 function App() {
+
+  const [isLoading, setLoading] = useState(true);
+
+  setTimeout(
+    ()=> setLoading(false), 3000
+  );
+
+
+  const products = [
+    { id: 1, name: "Laptop", price: 80000, inStock: true },
+    { id: 2, name: "Mouse", price: 800, inStock: false },
+    { id: 3, name: "Keyboard", price: 1500, inStock: true }
+  ];
+
+  const productsEmpty = [];
+
   return (
     <>
-      <h2>This is App.js</h2>
+      <h2>Question 04 OUTPUT</h2>
+      {isLoading ? <Loader/> : <ProductList products={products}/>}
     </>
   )
 }
---
+++ src/components/Products/ProductList.jsx
@@ -0,0 +1,15 @@
+import ProductCard from "./ProductCard";
+
+export default function ProductList({products}){
+  
+  return(
+    <section>
+      {
+        products.length>0 
+        ? products.map(product => <ProductCard key={product.id} {...product}/>) 
+        : <p>No products available</p> 
+      } 
+    </section>
+  );  
+
+}
