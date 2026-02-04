0aa4c2aead791e2d200fb0d491454b6fd26929df
Mimansa Joshi <mimansaj@vconstruct.in>
2026-02-04 11:26:17 +0530

    changes: corrected the required changes as mentioned in pull request

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
-import Header from './components/Header'
+import './App.css';
+import Header from './components/Header.jsx';
+
+const HEADER = "React Fundamentals Training";
 
 function App() {
   return (
     <>
-      <Header heading="React Fundamentals Training"/>
+      <Header heading={HEADER}/>
       <p>Welcome to React</p>
     </>
-  )
+  );
 }
 
 export default App;
