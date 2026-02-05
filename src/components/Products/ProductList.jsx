94958544c3bd378f3fc304e72cc0c9fc20160692
Mimansa Joshi <mimansaj@vconstruct.in>
2026-02-05 12:35:52 +0530

    solution: updates in the solution: added hook useEffect

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -1,15 +1,13 @@
 import ProductCard from "./ProductCard";
 
-export default function ProductList({products}){
-  
-  return(
+export default function ProductList({ products }) {
+  return (
     <section>
-      {
-        products.length>0 
-        ? products.map(product => <ProductCard key={product.id} {...product}/>) 
-        : <p>No products available</p> 
-      } 
+      {products.length > 0 ? (
+        products.map((product) => <ProductCard key={product.id} {...product} />)
+      ) : (
+        <p>No products available</p>
+      )}
     </section>
-  );  
-
+  );
 }
